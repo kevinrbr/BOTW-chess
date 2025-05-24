4db3803f01349649c6d235347c66a01cd08636e9
Sirius Nottin <30043962+siriusnottin@users.noreply.github.com>
2025-05-24 20:47:56 +0200

    feat: add SignUpForm component and integrate it into HomeView

--- src/home/views/HomeView.vue
+++ src/home/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import TextOnBackgroundVue from '@/home/components/TextOnBackground.vue';
+import SignUpForm from '../components/SignUpForm.vue';
 import ChessCarousel from '@/home/components/ChessCarousel.vue';
 import chess1 from '@/common/assets/chess1.png';
 import chess2 from '@/common/assets/chess2.png';
@@ -9,18 +10,14 @@ import chess2 from '@/common/assets/chess2.png';
 <template>
   <div class="bento">
     <div class="bento__one"></div>
-    <div class="bento__two"></div>
+    <div class="bento__two">
+      <SignUpForm />
+    </div>
     <div class="bento__three">
-     <TextOnBackgroundVue 
-        text="Alapine More Than a Club A True Passion for Chess"
-        :image="chess1"  
-     />
+      <TextOnBackgroundVue text="Alapine More Than a Club A True Passion for Chess" :image="chess1" />
     </div>
     <div class="bento__four">
-      <TextOnBackgroundVue 
-        text="Step into the World of Alapine Chess Club"
-        :image="chess2"  
-      />
+      <TextOnBackgroundVue text="Step into the World of Alapine Chess Club" :image="chess2" />
     </div>
     <div class="bento__five"></div>
     <div class="bento__six">
@@ -72,7 +69,7 @@ import chess2 from '@/common/assets/chess2.png';
     background-image: url('@/common/assets/goat.png');
     background-size: cover;
   }
-  
+
   &__six {
     border-radius: pxToRem(24px);
     grid-area: six;
